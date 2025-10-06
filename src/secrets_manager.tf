7f09d4316452e8c352b41475ef11c308db8637ae
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-06 06:22:25 -0300

    feat: add AWS Secrets Manager and Service Discovery resources for Gaia project

---
+++ src/secrets_manager.tf
@@ -0,0 +1,22 @@
+resource "random_password" "postgres_db_master_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&()*+,-.:;<=>?[]^{|}~"
+}
+
+resource "aws_secretsmanager_secret" "postgres_db_credentials" {
+  name = "/${terraform.workspace}/postgres_db/credentials"
+
+  tags = {
+    IAC = true
+  }
+}
+
+data "aws_secretsmanager_secret" "clerk_credentials" {
+  name = "/${terraform.workspace}/clerk/credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "postgres_db_password_version" {
+  secret_id     = aws_secretsmanager_secret.postgres_db_credentials.id
+  secret_string = random_password.postgres_db_master_password.result
+}
