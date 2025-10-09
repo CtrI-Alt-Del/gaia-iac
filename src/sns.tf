39e4687c44a0d351fed45ed28381c4523d5708e1
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-09 09:16:27 -0300

    chore: #gaia-20 add AWS SNS resources for budget alerts including topic, subscription, and policy in sns.tf

---
+++ src/sns.tf
@@ -0,0 +1,18 @@
+resource "aws_sns_topic" "budget_alerts" {
+  name = "${terraform.workspace}-budget-alerts"
+
+  tags = {
+    IAC = true
+  }
+}
+
+resource "aws_sns_topic_subscription" "email_target" {
+  topic_arn = aws_sns_topic.budget_alerts.arn
+  protocol  = "email"
+  endpoint  = "joaopcarvalho@gmail.com"
+}
+
+resource "aws_sns_topic_policy" "budget_policy" {
+  arn    = aws_sns_topic.budget_alerts.arn
+  policy = data.aws_iam_policy_document.sns_topic_policy_doc.json
+}
