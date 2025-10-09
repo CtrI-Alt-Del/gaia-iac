95d44db05d5304ee506728533fdf6508e79d386f
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-09 09:16:37 -0300

    chore: #gaia-20 add AWS Budgets resource for monthly cost tracking with notifications in budgets.tf

---
+++ src/budgets.tf
@@ -0,0 +1,28 @@
+resource "aws_budgets_budget" "monthly_cost" {
+  name              = "${terraform.workspace}-monthly-cost-budget"
+  budget_type       = "COST"
+  limit_amount      = "100.0"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+  time_period_start = "2025-10-01_00:00"
+
+  notification {
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 100
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "ACTUAL"
+    subscriber_sns_topic_arns = [aws_sns_topic.budget_alerts.arn]
+  }
+
+  notification {
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 100
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "FORECASTED"
+    subscriber_sns_topic_arns = [aws_sns_topic.budget_alerts.arn]
+  }
+
+  tags = {
+    IAC = true
+  }
+}
