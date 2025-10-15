64cb9e8743b073fdb645138aa7b2bf6c123dd3b8
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-15 13:56:43 -0300

    feat: add gaia-109 AWS Secrets Manager resources for DocumentDB credentials and password generation

--- src/secrets_manager.tf
+++ src/secrets_manager.tf
@@ -20,3 +20,18 @@ resource "aws_secretsmanager_secret_version" "postgres_db_password_version" {
   secret_id     = aws_secretsmanager_secret.postgres_db_credentials.id
   secret_string = random_password.postgres_db_master_password.result
 }
+
+resource "random_password" "docdb_master_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&()*+,-./:;<=>?@[]^_`{|}~"
+}
+
+resource "aws_secretsmanager_secret" "docdb_credentials" {
+  name = "${terraform.workspace}/docdb/credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "docdb_credentials_version" {
+  secret_id     = aws_secretsmanager_secret.docdb_credentials.id
+  secret_string = random_password.docdb_master_password.result
+}
