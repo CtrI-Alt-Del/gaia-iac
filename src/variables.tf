7a40744a4dcb96d16ee8a8da2fdf5e5a3f42781b
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-15 13:56:01 -0300

    feat: gaia-109 add variables for Gaia Collector and RDS configuration in Terraform

--- src/variables.tf
+++ src/variables.tf
@@ -55,11 +55,60 @@ variable "gaia_server_container_port" {
 variable "gaia_server_container_cpu" {
   description = "Unidades de CPU para alocar à tarefa Fargate para a aplicação Gaia Server"
   type        = number
-  default     = 512 # 0.5 vCPU
+  default     = 512
 }
 
 variable "gaia_server_container_memory" {
   description = "Memória em MiB para alocar à tarefa Fargate para a aplicação Gaia Server"
   type        = number
-  default     = 1024 # 1 GB
+  default     = 1024
+}
+
+
+variable "gaia_collector_container_name" {
+  description = "Nome do contêiner da aplicação Gaia Collector"
+  type        = string
+  default     = "gaia-collector"
+}
+
+variable "gaia_collector_container_cpu" {
+  description = "Unidades de CPU para alocar à tarefa Fargate para a aplicação Gaia Collector"
+  type        = number
+  default     = 256
+}
+
+variable "gaia_collector_container_memory" {
+  description = "Memória em MiB para alocar à tarefa Fargate para a aplicação Gaia Collector"
+  type        = number
+  default     = 512
+}
+
+variable "gaia_panel_desired_count" {
+  description = "Número de instâncias desejadas para o Gaia Panel."
+  type        = number
+  default     = 1
+}
+
+variable "gaia_server_desired_count" {
+  description = "Número de instâncias desejadas para o Gaia Server."
+  type        = number
+  default     = 1
+}
+
+variable "gaia_collector_desired_count" {
+  description = "Número de instâncias desejadas para o Gaia Collector."
+  type        = number
+  default     = 1
+}
+
+variable "rds_instance_class" {
+  description = "Classe da instância para o banco de dados RDS (ex: db.t4g.micro)."
+  type        = string
+  default     = "db.t4g.micro"
+}
+
+variable "rds_multi_az" {
+  description = "Habilita a alta disponibilidade (Multi-AZ) para o RDS."
+  type        = bool
+  default     = false
 }
