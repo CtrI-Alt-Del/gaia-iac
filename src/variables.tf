9abaf8d75c99c49694d9982064ed6e7dc25c375d
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-21 22:51:31 -0300

    feat: gaia-108 add variables for elasticache

--- src/variables.tf
+++ src/variables.tf
@@ -28,6 +28,12 @@ variable "gaia_panel_container_memory" {
   default     = 1024 # 1 GB
 }
 
+variable "gaia_panel_desired_count" {
+  description = "Número de instâncias desejadas para o Gaia Panel."
+  type        = number
+  default     = 1
+}
+
 variable "gaia_server_app_mode" {
   description = "Modo de execução da aplicação (ex: development, staging,production)."
   type        = string
@@ -61,5 +67,56 @@ variable "gaia_server_container_cpu" {
 variable "gaia_server_container_memory" {
   description = "Memória em MiB para alocar à tarefa Fargate para a aplicação Gaia Server"
   type        = number
-  default     = 1024 # 1 GB
+  default     = 1024
+}
+
+variable "gaia_server_desired_count" {
+  description = "Número de instâncias desejadas para o Gaia Server."
+  type        = number
+  default     = 1
+}
+
+
+variable "gaia_collector_container_name" {
+  description = "Nome do contêiner da aplicação Gaia Collector"
+  type        = string
+  default     = "gaia-collector"
+}
+
+variable "gaia_collector_container_cpu" {
+  description = "Unidades de CPU para alocar à tarefa Fargate para a aplicação Gaia Collector"
+  type        = number
+  default     = 256
+}
+
+variable "gaia_collector_container_memory" {
+  description = "Memória em MiB para alocar à tarefa Fargate para a aplicação Gaia Collector"
+  type        = number
+  default     = 512
+}
+
+variable "gaia_collector_desired_count" {
+  description = "Número de instâncias desejadas para o Gaia Collector."
+  type        = number
+  default     = 1
+}
+
+
+
+variable "rds_instance_class" {
+  description = "Classe da instância para o banco de dados RDS (ex: db.t4g.micro)."
+  type        = string
+  default     = "db.t4g.micro"
+}
+
+variable "rds_multi_az" {
+  description = "Habilita a alta disponibilidade (Multi-AZ) para o RDS."
+  type        = bool
+  default     = false
+}
+
+variable "elasticache_instance_class" {
+  description = "Classe da instância para o banco de dados ElastiCache (ex: cache.t4g.micro)."
+  type        = string
+  default     = "cache.t4g.micro"
 }
