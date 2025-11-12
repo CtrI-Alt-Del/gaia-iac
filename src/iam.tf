799e0564f4af0433d54f13ca7d5f621a5a980e2b
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-12 01:10:54 -0300

    feat: #GAIA-167 add ECS instance role and policy attachment for EC2 instances; create instance profile for ECS integration

--- src/iam.tf
+++ src/iam.tf
@@ -324,3 +324,30 @@ resource "aws_iam_role_policy_attachment" "ecs_task_role_attaches_exec_policy" {
   role       = aws_iam_role.ecs_task_role.name
   policy_arn = aws_iam_policy.ecs_exec_policy.arn
 }
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "${terraform.workspace}-ecs-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+
+  tags = {
+    IAC = true
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "${terraform.workspace}-ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
